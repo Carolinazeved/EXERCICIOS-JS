41690b06439ee65deb2e9ea2dcbb848b0b34a432
Carolin Azevedo <carolinevolney@hotmail.com>
2024-12-28 17:06:34 -0300

    reordenar

--- FORMULARIO/script.js
+++
@@ -1,8 +0,0 @@
-
-function capturarValores() {
-    const nome = document.getElementById('campoNome').value;
-    const idade = document.getElementById('campoIdade').value;
-
- document.getElementById('mostraNome').textContent = `Nome: ${nome}, Idade: ${idade}`;
-    console.log(`Nome: ${nome}, Idade: ${idade}`);
-}
